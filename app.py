50b1394021a6217f65514cd69dd96ac5cd0e38eb
Hadryanns <hadryan2007nascimentosantos@gmail.com>
2025-07-25 23:42:30 -0300

    Feat: Arrumando a tela principal

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('main.html')
+
+@app.route('/verse')
+def verse():
+    return render_template('verse.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
